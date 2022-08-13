b077d8a91e3b77bbd8cb8267bc380345dd5ae5b4
martincorona007 <martincorona007@hotmail.com>
2022-08-13 10:17:57 -0500

    08/13/2022

--- E10/index.js
+++ E10/index.js
@@ -1,7 +1,14 @@
-let array1 = [1,2,3,4,5];
-let array2 = [1,2,3,4,5];
-//console.log(array1.isArray(array1));
-console.log(Array.isArray(array1));
-  
-array1.push(4);
-console.log(array1);
+const obj1 = {
+  name: "Franki",
+  lastName: "Rives",
+  printInfo: function(){
+    return `Your ${this.name} and ${this.lastName}`
+  }
+}
+const moreInfo = function(age){
+  console.log(`${this.printInfo()} and ${age} yeas`)
+}
+const print = moreInfo.bind(obj1)
+print(3)
+const print2 = moreInfo.call(obj1,5)
+const print3 = moreInfo.apply(obj1,[50])
